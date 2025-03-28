e4ffe3bbca741a893492dc28141e75d93a864d9f
gomminjae <gommj0611@naver.com>
2025-03-28 16:20:22 +0900

    Initial Commit

---
+++ MOLDIV-Filter/MOLDIV_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOLDIV_FilterApp.swift
+//  MOLDIV-Filter
+//
+//  Created by 권민재 on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MOLDIV_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
